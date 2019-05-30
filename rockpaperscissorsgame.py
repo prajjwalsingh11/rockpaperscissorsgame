2f27a91d8642a53a7a7632904562c57daab7f7fd
Prajjwal Singh <39522894+prajjwalsingh11@users.noreply.github.com>
2019-05-30 11:42:28 +0530

    Add files via upload

---
+++ rockpaperscissorsgame.py
@@ -0,0 +1,37 @@
+from random import randint
+
+player = input('rock (r), paper (p) or scissors (s)?')
+print(player, 'vs' , end=' ')
+
+chosen = randint(1,3)
+
+if chosen == 1:
+    computer = 'r'
+
+elif chosen == 2:
+    computer = 'p'
+
+else:
+    computer = 's'
+print(computer)
+
+if player == computer:
+    print('DRAW!')
+
+elif player == 'r' and computer == 's':
+    print('Player Wins!')
+
+elif player == 'r' and computer == 'p':
+    print('Computer Wins!')
+
+elif player == 'p' and computer == 's':
+    print('Computer Wins!')
+
+elif player == 'p' and computer == 'r':
+    print('Player Wins!')
+
+elif player == 's' and computer == 'p':
+    print('Player Wins!')
+
+elif player == 's' and computer == 'r':
+    print('Computer Wins!')
